b217cfa5f5c8c089ed08b58e34b60964c6036a1c
duonghan <20155296@student.hust.edu.vn>
2019-02-02 15:54:39 +0700

    added owners list and udpate controller

--- pet-clinic-web/src/main/java/com/duonghv/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/duonghv/springpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.duonghv.springpetclinic.controllers;
 
+import com.duonghv.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,8 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
